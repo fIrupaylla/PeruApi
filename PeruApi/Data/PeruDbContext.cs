a1464f95e4bc514c8e92b32bd3c6786d5411d39a
Fiorella Irupaylla <Fiorella Irupaylla>
2024-04-27 17:48:07 -0500

    Agregar archivos de proyecto.

---
+++ PeruApi/Data/PeruDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PeruApi.Data;
+
+public partial class PeruDbContext : DbContext
+{
+    public PeruDbContext()
+    {
+    }
+
+    public PeruDbContext(DbContextOptions<PeruDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<People> People { get; set; }
+
+    public virtual DbSet<Product> Product { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=PeruDB;User=sa;Pwd=123456789;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<People>(entity =>
+        {
+            entity.Property(e => e.Dni).HasMaxLength(20);
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.Fullname).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Description).HasMaxLength(100);
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
